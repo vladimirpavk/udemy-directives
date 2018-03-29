2d3a2057fd41d557b412d05d1ec4b2bb6a64765c
Vladimir Pavković <vladimirpavk@gmail.com>
2018-03-29 15:47:54 +0200

    custom directives

---
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -0,0 +1,34 @@
+import { Directive, OnInit, Renderer2, ElementRef, HostListener, HostBinding, Input } from '@angular/core';
+
+@Directive({
+  selector: '[appBetterHighlight]'
+})
+export class BetterHighlightDirective implements OnInit{
+
+  @Input() highlightColor: string = "blue";
+  @Input() defaultColor: string = "transparent";
+
+  @HostBinding('style.backgroundColor') backgroundColor: string;
+
+  constructor(private elementRef: ElementRef, private renderer: Renderer2) {
+
+   }
+
+  ngOnInit(){
+    //this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
+    this.backgroundColor=this.defaultColor;
+  }
+
+  @HostListener('mouseenter') mouseEnter(event: Event){
+    //this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
+    //this.backgroundColor = 'blue';
+    this.backgroundColor = this.highlightColor;
+  }
+
+  @HostListener('mouseleave') mouseLeave(event: Event){
+    //this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'transparent');
+    //this.backgroundColor = 'transparent';
+    this.backgroundColor = this.defaultColor;
+  }
+
+}
